8d622e05fccca24179dc237f4397406566e3d534
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-11 22:28:15 +0900

    add: section9が完了しました

---
+++ src/lesson/esModules/defaultImport.js
@@ -0,0 +1,9 @@
+// ここにコードを書きながら確認しましょう！
+// import dummyFn from "./defaultExport.js";
+
+// console.log(dummyFn("Gizumo"));
+
+// 練習問題
+// 問題 2
+import defaultFn from "./defaultExport.js";
+console.log(defaultFn());
