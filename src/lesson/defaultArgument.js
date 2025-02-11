03c0c909769d7fae84e03f4b891c61227ca98fae
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-11 16:07:53 +0900

    Add: 問題1完了

---
+++ src/lesson/defaultArgument.js
@@ -0,0 +1,29 @@
+// ここにコードを書きながら確認しましょう！
+function nameFn(name) {
+  return `Gizumo ${name}`;
+}
+console.log(nameFn("taro"));
+
+function argFn(arg = "Hello") {
+  console.log(arg);
+}
+argFn();
+
+function totalPrice(price, tax = 1.1) {
+  return price * tax;
+}
+console.log(totalPrice(1000, 1.1));
+console.log(totalPrice(1000));
+
+// function argFn(arg = "Hello") {
+//   console.log(arg);
+// }
+// argFn(undefined);
+
+// 練習問題
+// 問題 1
+function discountRateCalc(price, rate = 5) {
+  return price * (1 - rate * 0.01);
+}
+console.log(discountRateCalc(4500));
+console.log(discountRateCalc(6000, 20));
