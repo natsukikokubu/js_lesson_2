c3ccef5b6e7dbbd3b23a932dfe9b945607bcd907
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-11 18:00:47 +0900

    fix: 修正

--- src/lesson/defaultArgument.js
+++ src/lesson/defaultArgument.js
@@ -22,8 +22,8 @@ console.log(totalPrice(1000));
 
 // 練習問題
 // 問題 1
-function discountRateCalc(price, rate = 5) {
-  return price * (1 - rate * 0.01);
+function discountRateCalc(price, rate = 0.05) {
+  return price * (1 - rate);
 }
 console.log(discountRateCalc(4500));
-console.log(discountRateCalc(6000, 20));
+console.log(discountRateCalc(6000, 0.2));
