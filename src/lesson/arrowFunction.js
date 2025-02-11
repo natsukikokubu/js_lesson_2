ecc6f67cc45273dd5164923ce6fcb27ee72fdbe4
natsukikokubu <natsukikokubu3@gmail.com>
2025-02-11 18:44:48 +0900

    section7 アロー関数が完了しました

---
+++ src/lesson/arrowFunction.js
@@ -0,0 +1,15 @@
+// ここにコードを書きながら確認しましょう！
+
+// const arrowFn = (num) => {
+//   return num * 10;
+// };
+const arrowFn = (num) => num * 10;
+
+// 練習問題
+// 問題 1
+// const testFn = function (name) {
+//   return `Gizumo ${name}`;
+// };
+
+const testFn = (name) => `Gizumo ${name}`;
+console.log(testFn("gizumo"));
